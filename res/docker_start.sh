f0fb9c58f1181f1387144b313577c6c3141e157e
Ebenolt <ebenolt@hotmail.fr>
2021-11-15 23:18:56 +0100

    .

--- res/docker_start.sh
+++ res/docker_start.sh
@@ -4,5 +4,5 @@ pipenv run python3 manage.py makemigrations
 pipenv run python3 manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('django_admin', 'admin@localhost', 'pass')" | python manage.py shell
 token=$(python manage.py drf_create_token django_admin | awk '{print $3}')
-sed -i -e "s/.*token.*/token = $token/g" config_default.ini
+sed -i -e "s/.*token.*/token = $token/g" config.ini
 pipenv run /bin/sh /home/access-pc/start.sh
