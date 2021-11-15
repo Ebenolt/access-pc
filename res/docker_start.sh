157b9e7d4ad6d34c47831b849ee7b88c70ae7891
Ebenolt <ebenolt@hotmail.fr>
2021-11-15 23:49:38 +0100

    .

--- res/docker_start.sh
+++ res/docker_start.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+echo "STEP 1"
 cd /home/access-pc/
+echo "STEP 2"
 pipenv run python3 manage.py makemigrations
+echo "STEP 3"
 pipenv run python3 manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('django_admin', 'admin@localhost', 'pass')" | python manage.py shell
-token=$(python manage.py drf_create_token django_admin | awk '{print $3}')
+echo "STEP 4"
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('django_admin', 'admin@localhost', 'pass')" | pipenv run python3 manage.py shell
+echo "STEP 5"
+token=$(python3 manage.py drf_create_token django_admin | awk '{print $3}')
+echo "STEP 6"
 sed -i -e "s/.*token.*/token = $token/g" res/config.ini
+echo "STEP 7"
+sleep 1000
+echo "STEP 8"
 pipenv run /bin/sh /home/access-pc/start.sh
