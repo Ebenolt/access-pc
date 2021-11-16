7d2da2b86e0090225895392e5f9e52e58b08ac80
Ebenolt <ebenolt@hotmail.fr>
2021-11-16 13:52:01 +0100

    Working docker compose !

--- res/docker_start.sh
+++ res/docker_start.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
+cd /home/access-pc/res
+$(grep config.ini | sed 's/ *= */=/g' | sed 's/\r//g' > source)
 cd ..
-$(grep = config.ini | sed 's/ *= */=/g' | sed 's/\r//g' > source)
 sleep 60
 echo "1. Preparing Migrations"
 pipenv run python3 manage.py makemigrations
 echo "2. Applying Migrations"
 pipenv run python3 manage.py migrate
 echo "3. Creating superuser / token"
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('${admin_username}', '${admin_email}', '${admin_password}')" | pipenv run python3 manage.py shell
-token=$(pipenv run python3 manage.py drf_create_token ${admin_username} | awk '{print $3}')
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('django_admin', '"+${admin_email}+"', '"+${admin_password}+"')" | pipenv run python3 manage.py shell
+admin_username = ${admin_username}
+token=$(pipenv run python3 manage.py drf_create_token django_admin | awk '{print $3}')
 echo "4. Editing configuration"
 cd res
 cp config.ini temp.ini
