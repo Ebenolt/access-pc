10c91cbdf89d56b082f2037761c74c2c9ecc9264
Ebenolt <ebenolt@hotmail.fr>
2021-11-16 09:16:04 +0100

    API Account in config.ini

--- res/docker_start.sh
+++ res/docker_start.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-cd /home/access-pc/
+cd ..
+source <(grep = config.ini | sed 's/ *= */=/g' | sed 's/\r//g')
 sleep 60
 echo "1. Preparing Migrations"
 pipenv run python3 manage.py makemigrations
 echo "2. Applying Migrations"
 pipenv run python3 manage.py migrate
 echo "3. Creating superuser / token"
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('django_admin', 'admin@localhost', 'pass')" | pipenv run python3 manage.py shell
-token=$(pipenv run python3 manage.py drf_create_token django_admin | awk '{print $3}')
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('${admin_username}', '${admin_email}', '${admin_password}')" | pipenv run python3 manage.py shell
+token=$(pipenv run python3 manage.py drf_create_token ${admin_username} | awk '{print $3}')
 echo "4. Editing configuration"
 cd res
 cp config.ini temp.ini
